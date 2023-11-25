03877a88f99f5e151f748bcf134689a5cd2e1a88
Brahim Abdelmomen <118140084+slyvalomas@users.noreply.github.com>
2023-11-25 07:57:57 +0100

    Update first.js

--- code/js/first.js
+++ code/js/first.js
@@ -1,133 +1,133 @@
 //! str 1st lesson
-// const variable="i need to chill"
+const variable="i need to chill"
 
-// console.log("machaaal dont leave me here!".length);
+console.log("machaaal dont leave me here!".length);
 
-// console.log(variable.charAt(1));
+console.log(variable.charAt(1));
 
-// console.log(variable.lastIndexOf("brahim"));
+console.log(variable.lastIndexOf("brahim"));
 
-// console.log(variable.slice(7,));
+console.log(variable.slice(7,));
 
-// console.log(variable.slice(0,6).toUpperCase());
+console.log(variable.slice(0,6).toUpperCase());
 
-// console.log(variable.split(" "))
-// console.log("maichaaal help me".split(" "));
+console.log(variable.split(" "))
+console.log("maichaaal help me".split(" "));
 
-// //! numbers
-// const number_float=19.25;
-// const number_str="19";
-// const number_int=19
-// console.log(number_str);
-// console.log(number_int);
-// console.log(number_float);
-// console.log(number_int===number_str)
-// console.log(number_int===Number(number_str))
-// console.log(number_int+ Number(number_str))
-// console.log(String(number_int)===number_str)
-// console.log(Number.isInteger(number_str))
-// console.log(Number.isInteger(Number(number_str)))
-// ! math
-// console.log(Math.E) 
-//console.log(Math.LNE)//should be 1 ?? ln(e)=1 e**ln=1
-// console.log(Math.LN10)
-// console.log(Math.trunc(Math.E))
-// console.log(Math.round(Math.E ))
-// console.log(Math.ceil(Math.E))
-// console.log(Math.floor(Math.E))
-// console.log(Math.pow(2,4))
-// console.log(2**4)
-// console.log(Math.min(5,7,82,8,17,5,1,8,2,75,48,))
-// console.log(Math.max(55,74,22,85,1,2,51,851,521,81,48,2,55,82,))
-// console.log(Math.abs(554,8,85,8,2,8555))
-// console.log(Math.random())
-// console.log(Math.random())
+//! numbers
+const number_float=19.25;
+const number_str="19";
+const number_int=19
+console.log(number_str);
+console.log(number_int);
+console.log(number_float);
+console.log(number_int===number_str)
+console.log(number_int===Number(number_str))
+console.log(number_int+ Number(number_str))
+console.log(String(number_int)===number_str)
+console.log(Number.isInteger(number_str))
+console.log(Number.isInteger(Number(number_str)))
+//! math
+console.log(Math.E) 
+console.log(Math.LNE)//should be 1 ?? ln(e)=1 e**ln=1
+console.log(Math.LN10)
+console.log(Math.trunc(Math.E))
+console.log(Math.round(Math.E ))
+console.log(Math.ceil(Math.E))
+console.log(Math.floor(Math.E))
+console.log(Math.pow(2,4))
+console.log(2**4)
+console.log(Math.min(5,7,82,8,17,5,1,8,2,75,48,))
+console.log(Math.max(55,74,22,85,1,2,51,851,521,81,48,2,55,82,))
+console.log(Math.abs(554,8,85,8,2,8555))
+console.log(Math.random())
+console.log(Math.random())
 
-// console.log(Math.floor(Math.random()*10)+1)
-// console.log(Math.floor(Math.random()*100)+1)
-// ! first challenge
-// //1
-// const name1 ="brahim abdelmoumen"
-// console.log(name1.charAt((Math.floor(Math.random()*17)),))
-////2
-// console.log(name1.charAt(Math.floor(Math.random()*name1.length)))
+console.log(Math.floor(Math.random()*10)+1)
+console.log(Math.floor(Math.random()*100)+1)
+//! first challenge
+//1
+const name1 ="brahim abdelmoumen"
+console.log(name1.charAt((Math.floor(Math.random()*17)),))
+//2
+console.log(name1.charAt(Math.floor(Math.random()*name1.length)))
 
-// let name2= "brahim";
-// let hi;
-// let ban = false
-// let chrack=false
-// if(ban){
-//     hi="go away"
-// }   else if (name2 && chrack) {
-//     hi="hi " +name2
-// }  else if (name2) {
-//     hi="hi mr " +name2 
-// }     else{
-//     hi="hello you"
-// }
+let name2= "brahim";
+let hi;
+let ban = false
+let chrack=false
+if(ban){
+    hi="go away"
+}   else if (name2 && chrack) {
+    hi="hi " +name2
+}  else if (name2) {
+    hi="hi mr " +name2 
+}     else{
+    hi="hello you"
+}
 
-// let h2;
-// let ban1 = true
+let h2;
+let ban1 = true
 
-// if(ban1){
-//     h2="go away"
-// }   else if (name2 && chrack) {
-//     h2="hi " +name2
-// }  else if (name2) {
-//     h2="hi mr " +name2 
-// }     else{
-//     h2="hello you"
-// }
+if(ban1){
+    h2="go away"
+}   else if (name2 && chrack) {
+    h2="hi " +name2
+}  else if (name2) {
+    h2="hi mr " +name2 
+}     else{
+    h2="hello you"
+}
 
-// let h3;
-// let ban2 = false
-// let chrack1=true
-// if(ban2){
-//     h3="go away"
-// }   else if (name2 && chrack1) {
-//     h3="hi " +name2
-// }  else if (name2) {
-//     h3="hi mr " +name2 
-// }     else{
-//     h3="hello you"
-// }
+let h3;
+let ban2 = false
+let chrack1=true
+if(ban2){
+    h3="go away"
+}   else if (name2 && chrack1) {
+    h3="hi " +name2
+}  else if (name2) {
+    h3="hi mr " +name2 
+}     else{
+    h3="hello you"
+}
 
-// console.log(hi)
-// console.log(h2)
-// console.log(h3)
+console.log(hi)
+console.log(h2)
+console.log(h3)
 
-// let cdf_codding=20;
-// let fall;
-// if (cdf_codding>=360){
-//     fall="yes keep going "
+let cdf_codding=20;
+let fall;
+if (cdf_codding>=360){
+    fall="yes keep going "
     
-// }
-//     else if (cdf_codding<360){
-//         fall="you need more"
-//     }
-// console.log(fall)
-// let points=14.58;
-// let grade;
-// let college=true;
-// if (points>=16){
-//     grade='très bien'
-// }
-// else if (points>=14){
-//     grade='bien'
-// }
-// else if (points>=12){
-//     grade='assez bien'
-// }
-// else if (points>=10){
-//     grade='acceptables'
-//     if (college){
-//         grade='Échoué'
-//     }
-// }
-// else if (points<10){
-//     grade='Échoué'
-// }
-// console.log(grade)
+}
+    else if (cdf_codding<360){
+        fall="you need more"
+    }
+console.log(fall)
+let points=14.58;
+let grade;
+let college=true;
+if (points>=16){
+    grade='très bien'
+}
+else if (points>=14){
+    grade='bien'
+}
+else if (points>=12){
+    grade='assez bien'
+}
+else if (points>=10){
+    grade='acceptables'
+    if (college){
+        grade='Échoué'
+    }
+}
+else if (points<10){
+    grade='Échoué'
+}
+console.log(grade)
 let me = "sizer";
 let computer = "sizer" ;
 if (me === computer){
